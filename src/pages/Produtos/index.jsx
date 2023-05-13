a6b1f764b0a947a54002e05b0ea067702f309955
IgorQroz <Igorqueirozdossantoslima_@hotmail.com>
2023-05-13 17:00:19 -0300

    produtos

--- src/pages/Produtos/index.jsx
+++ src/pages/Produtos/index.jsx
@@ -1,38 +1,35 @@
-import estilo from './styles.module.scss'
-import Filtrar from '../../components/MenuFiltrar'
-import Produtos from '../../components/ProdutoCard'
-import Ordenar from '../../components/MenuOrdenar'
+import estilo from "./styles.module.scss";
+import Filtrar from "../../components/MenuFiltrar";
+import Produtos from "../../components/ProdutoCard";
+import Ordenar from "../../components/MenuOrdenar";
+import { useEffect, useState } from "react";
 export default function ProdutoHome() {
-    return (
-        <div className={estilo.teste}>
-            <div className={estilo.container}>
-                <div className={estilo.Ordenar}>
-                    <Ordenar />
-                </div>
-                <div className={estilo.containerProdutos}>
-                    <div className={estilo.menuFiltrar}>
-                        <Filtrar />
-                    </div>
-                    <div className={estilo.produtos}>
-                        <Produtos />
-                        <Produtos />
-                        <Produtos />
-                        <Produtos />
-                        <Produtos />
-                        <Produtos />
-                        <Produtos />
-                        <Produtos />
-                        <Produtos />
-                        <Produtos />
-                        <Produtos />
-                        <Produtos />
-                        <Produtos />
-                        <Produtos />
-                        <Produtos />
-                    </div>
+  const [produtos, setProdutos] = useState([]);
+  useEffect(() => {
+    console.log(1);
+    fetch("https://backend-dc-igor.onrender.com/produtos")
+      .then((res) => res.json())
+      .then((data) => setProdutos(data));
+  }, []);
 
-                </div>
-            </div>
+  return (
+    <div className={estilo.teste}>
+      <div className={estilo.container}>
+        <div className={estilo.Ordenar}>
+          <Ordenar />
         </div>
-    )
+        <div className={estilo.containerProdutos}>
+          <div className={estilo.menuFiltrar}>
+            <Filtrar />
+          </div>
+          <div className={estilo.produtos}>
+            {produtos.map((produto) => (
+              <Produtos 
+              key={produto.id} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
