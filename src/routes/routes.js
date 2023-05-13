3d4e42826a9b5da728d18d3c270f4d72a2543b3c
viniorc <vorc.tec@gmail.com>
2023-05-13 14:12:14 -0300

    mudança de home para inicial

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,4 @@
-export const route = [{ title: 'Home', route: '/' },
+export const route = [{ title: 'inicial', route: '/' },
 { title: 'Produtos', route: '/produtos' },
 { title: 'Categorias', route: '/categorias' },
 { title: 'Meus Pedidos', route: '/meuspedidos' },
